b1207b987e8424721418885b486b0dbfc2725b27
alhacen <alhaqhassan@gmail.com>
2025-01-01 02:34:01 +0530

    fix: converted social-hero-img to webp

--- components/SocialHero.tsx
+++ components/SocialHero.tsx
@@ -1,5 +1,5 @@
 import type { NextComponentType } from "next";
-import HeroImg from "../images/social-hero-img.png";
+import HeroImg from "../images/social-hero-img.webp";
 import HeroMobileImg from "../images/social-hero-mobile-img.png";
 import Image from "next/image";
 import Link from "next/link";
@@ -25,21 +25,21 @@ const SocialHero: NextComponentType = () => {
                 <ThemeButton className="w-auto">
                   Start Free Trial
                   <svg
-                      width="20"
-                      height="20"
-                      viewBox="0 0 20 20"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M7.5 5L12.5 10L7.5 15"
-                        stroke="white"
-                        strokeWidth="1.66667"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                </ThemeButton>              
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M7.5 5L12.5 10L7.5 15"
+                      stroke="white"
+                      strokeWidth="1.66667"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </ThemeButton>
               </Link>
               <Link href={"#"} passHref>
                 <a className="inline-block py-[10px] px-[14px] rounded-lg text-[#475467] text-[14px] font-[400] leading-[20px] tracking-[-0.006em] [box-shadow:0px_0px_0px_1px_#E1E4EA,_0px_1px_3px_0px_#0E121B1F] bg-white">
