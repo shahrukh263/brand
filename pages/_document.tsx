0ebc9e86fab0d2a624437a98595de1fce71c799f
Shahrukh Khan <61946634+shahrukh263@users.noreply.github.com>
2025-01-09 10:41:07 +0530

    favicon icon updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<link rel='icon' href='/icon.png' />
+					<link rel='icon' href='/favicon.svg' />
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 					<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
